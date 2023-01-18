113b2587076a88a2f749fd00dc8c30d2bc553b75
Olivier Churlaud <olivier.churlaud@cnes.fr>
2023-01-18 18:59:18 +0000

    [Tests] Better test framework

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -2,11 +2,30 @@
 
 TEST_DIR="$(dirname $(realpath ${BASH_SOURCE[0]}))"
 
-pushd ${TEST_DIR}
+pushd ${TEST_DIR} > /dev/null
 
-./00_init_workspace.sh
-./10_init_opsconf.sh
-./20_commits.sh
-./21_commits_hooks.sh
+scripts=$(cd $(dirname $0) ; ls * | grep -v 'env.sh' | grep -v 'run_tests.sh')
 
-popd
+if [ $# -ge 1 ] ; then
+    filter=$*
+else
+    filter=".*"
+fi
+
+
+for s in $scripts; do 
+    # Run all 0* files that are needed for initialization
+    if [[ $s =~ ^0.* ]] ; then
+        ./$s
+    else
+        # Run if at least a filter matches
+        for f in $filter ; do 
+            if [[ $s =~ .*$f.* ]] ; then
+                ./$s
+                break
+            fi
+        done
+    fi
+done
+
+popd > /dev/null
