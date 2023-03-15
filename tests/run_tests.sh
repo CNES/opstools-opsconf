8e290e1ba4b916465cdf7c52c1339fa2de9b5e9b
Olivier Churlaud <olivier.churlaud@cnes.fr>
2023-03-15 15:38:54 +0000

    minor: fix some warnings raised by the linter and add a comment

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -4,7 +4,8 @@ TEST_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 
 pushd "${TEST_DIR}" > /dev/null
 
-scripts=$(ls * | grep -v 'env.sh' | grep -v 'run_tests.sh')
+# shellcheck disable=SC2010
+scripts=$(ls ./* | grep -v 'env.sh' | grep -v 'run_tests.sh')
 
 if [ $# -ge 1 ] ; then
     filter=$*
@@ -15,14 +16,17 @@ fi
 
 for s in $scripts; do 
     # Run all 0* files that are needed for initialization
-    if [[ "$s" =~ ^0.* ]] ; then
-        ./"$s"
+    if [[ "$s" =~ ^./0.* ]] ; then
+        echo "[INFO] Run script $s"
+        "$s"
     else
         # Run if at least a filter matches
         for f in $filter ; do 
             if [[ "$s" =~ .*"$f".* ]] ; then
                 echo "[INFO] Run script $s"
-                ./"$s"
+                "$s"
+                # One filter has matched, the script has run
+                # no need to try the other ones (or we might run it several times)
                 break
             fi
         done
